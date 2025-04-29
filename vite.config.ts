a46d20437309c61688010c32da99de66611da998
Firas Zaazaa <firas.zaazaa@gmail.com>
2025-04-29 04:43:55 +0200

    added esnext and removed minifying on vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: "esnext", // pour autoriser top-level await
+    minify: false, // désactive minification (voir erreurs plus claires)
+  },
+});
