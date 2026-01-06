f651a0bca05c3e3f03f2d628ee2b351db5fc3650
METALNAVY9666 <metalnavy9666@gmail.com>
2026-01-06 17:28:50 +0100

    mise à jour du mail et amélioration d'un aspect sémantique

--- src/assets/components/Sections/Contact.tsx
+++ src/assets/components/Sections/Contact.tsx
@@ -1,14 +1,13 @@
+import socials from "../../data/socials.json";
+
 export default function Contact() {
+  const mailInfo = socials.filter((social) => social.id === "mail")[0];
+
   return (
     <div className="pb-5">
       <button
         style={{ fontWeight: 650 }}
-        onClick={() =>
-          window.open(
-            "mailto:firas.zaazaa@gmail.com?Subject=Prenons%20contact",
-            "_blank"
-          )
-        }
+        onClick={() => window.open(mailInfo.href, "_blank")}
         className="fs-1 border-white btn btn-dark text-white w-100 d-flex justify-content-center"
       >
         {" "}
